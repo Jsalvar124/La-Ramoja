e7eec935fe1af75649c104ffa4cb2b609a789129
Jsalvar124 <jsalvar124@gmail.com>
2024-11-14 11:48:23 -0500

    adding cors

---
+++ src/main/java/dh/backend/mojarra_tours/config/WebConfig.java
@@ -0,0 +1,17 @@
+package dh.backend.mojarra_tours.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow CORS for all paths
+                .allowedOrigins("*") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific methods
+                .allowedHeaders("*"); // Allow all headers
+    }
+}
